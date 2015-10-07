62cc3701c7d9c916101abe056fe2100bf41de165
nomasp <nomasp@outlook.com>
2015-10-07 11:37:20 +0800

    commit 1

---
+++ app/src/main/java/myapplication/nomasp/com/clock/MainActivity.java
@@ -0,0 +1,32 @@
+package myapplication.nomasp.com.clock;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class MainActivity extends Activity {
+
+    private TabHost tabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        tabHost.addTab(tabHost.newTabSpec("tabTimer").
+                setIndicator("时钟").setContent(R.id.tabTime));
+        tabHost.addTab(tabHost.newTabSpec("tabAlarm").
+                setIndicator("闹钟").setContent(R.id.tabAlarm));
+        tabHost.addTab(tabHost.newTabSpec("tabTimer").
+                setIndicator("计时器").setContent(R.id.tabTimer));
+        tabHost.addTab(tabHost.newTabSpec("tabStopWatch").
+                setIndicator("秒表").setContent(R.id.tabStopWatch));
+    }
+
+
+}
