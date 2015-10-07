513e3df65e8a23db63bc00038b7c46aa8a1b92dc
nomasp <nomasp@outlook.com>
2015-10-07 18:41:02 +0800

    commit 2

---
+++ app/src/main/java/myapplication/nomasp/com/clock/TimerView.java
@@ -0,0 +1,22 @@
+package myapplication.nomasp.com.clock;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by nomasp on 2015/10/07.
+ */
+public class TimerView extends LinearLayout {
+    public TimerView(Context context) {
+        super(context);
+    }
+
+    public TimerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TimerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
